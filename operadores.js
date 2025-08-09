b1b2dbeb014351bfcacaa60f313fceb1a87f4bd2
isabelacodderM <campoisabela07@gmail.com>
2025-08-09 09:37:15 -0500

    operadores

---
+++ operadores.js
@@ -0,0 +1,34 @@
+//suma, resta, multiplicacion, división
+//calcular valores de dos o mas elementos
+let num1 = 4;
+let num2 = 8;
+let suma = num1 + num2;
+console.log("el resultado de la suma es: " + suma);
+//contadores 
+let edad = 24;
+edad = edad + 1; //edad++
+console.log("la edad es: " + edad);
+//resta
+let rest1 = 7;
+let rest2 = 3;
+let resta = rest1 - rest2;  
+console.log("el resultado de la resta es: " + resta);
+resta = resta - 1;
+console.log("el resultado de la resta es: " + resta);
+//multiplicacion
+let mult1 = 2;
+let mult2 = 9;
+let mult = mult1 * mult2;
+console.log("el resultado de la multiplicacion es: " + mult);
+//división
+let div1 = 10;
+let div2 = 2;
+let div = div1 / div2;
+console.log("el resultado de la división es: " + div);
+//residuo
+let mod1 = 7;
+var mod2 = 5;
+let mod = mod1 % mod2;
+console.log("el resultado de la división es: " + mod);
+
+//operadores relacionales
---
+++ variables.js
@@ -0,0 +1,18 @@
+//string
+var sena = "ADSO";
+console.log(sena);
+//numeros
+let numero = 17;
+let numnegatuve = -4;
+let numdecimal = 11.74;
+//boleanos true - false
+var estoyestudiando = true;
+let estoyaburrido = false;
+//undefined
+var num;
+console.log(num);
+num = 0; 
+console.log(num);
+//null
+var sinNada = null;
+console.log(sinNada);
