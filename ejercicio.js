7cfacbdce1dc7d802acd75cd7d72feef2203564c
isabelacodderM <campoisabela07@gmail.com>
2025-08-09 12:03:55 -0500

    suma

---
+++ ejercicio.js
@@ -0,0 +1,8 @@
+let num1 = 25;
+let palabra1 = prompt("digite el primer numero para sumar");
+palabra1 = parseInt(palabra1);
+let palabra2 = prompt("digite la palabra 2 para sumar");
+palabra2 = parseInt(palabra2);
+let concatenar = palabra1 + palabra2;
+console.log(concatenar);    
+alert("el resultado de la suma es: " + concatenar);
--- prompt.js
+++ prompt.js
@@ -1,5 +1,7 @@
 let palabra1 = prompt("digite palabra 1 a concartenar");
+palabra1 = parseInt(palabra1);
 let palabra2 = prompt("digite palabra 2 a concartenar");
+palabra2 = parseInt(palabra2);
 let concatenar = palabra1 + palabra2;
 console.log(concatenar);    
 alert("esta es la palabra concartada: " + concatenar);
