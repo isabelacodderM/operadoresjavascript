06f7a715bedf15164527d56b02d32d0902b23ddb
isabelacodderM <campoisabela07@gmail.com>
2025-08-09 11:32:01 -0500

    multiplicacion

---
+++ multiplicacion.js
@@ -0,0 +1,5 @@
+let num1 = 4;
+var num2 = 8;
+let mult = num1 * num2;
+console.log(mult);
+alert(mult);
---
+++ prompt.js
@@ -0,0 +1,5 @@
+let palabra1 = prompt("digite palabra 1 a concartenar");
+let palabra2 = prompt("digite palabra 2 a concartenar");
+let concatenar = palabra1 + palabra2;
+console.log(concatenar);    
+alert("esta es la palabra concartada: " + concatenar);
