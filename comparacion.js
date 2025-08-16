3a06753f8347a273473f68464d883b3b5fe6629b
isabelacodderM <campoisabela07@gmail.com>
2025-08-15 21:28:19 -0500

    area

---
+++ comparacion.js
@@ -0,0 +1,17 @@
+console.log(8 > 2);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 == "2");
+console.log(2 === "hola");
+console.log(2 === "2");
+
+
+if (7%2 == 0) {
+    console.log("par");
+}
+if (7%2 == 1) {
+    console.log("impar");
+}
+
+
+7 % 2==0? console.log("par"): console.log("impar");
---
+++ cuadrado.js
@@ -0,0 +1,8 @@
+let lado = prompt("ingrese un lado de un cuadrado");
+lado = parseInt(lado);
+"lado"==="lado"? console.log("el lado es igual a el lado"): console.log("el lado es distinto a el lado");
+let area = lado * lado;
+console.log("el area del cuadrado es: " + area);
+alert("el area del cuadrado es: " + area);
+
+
