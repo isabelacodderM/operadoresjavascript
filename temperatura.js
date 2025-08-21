829307cd91d211a1014f7bc34e88281e4e972ebe
isabelacodderM <campoisabela07@gmail.com>
2025-08-20 21:12:52 -0500

    veinte

---
+++ temperatura.js
@@ -0,0 +1,2 @@
+let temp = parseInt(prompt("ingrese la temperatura"));
+(temp < 15)? alert("frio"): (temp > 15 && temp <25)? alert("agradable"): (temp >26 && temp<35)? alert("caluroso"): (temp>35)? alert("muy caluroso"): alert("digite un numero");
--- tresNumeros.js
+++ tresNumeros.js
@@ -1,4 +1,7 @@
 let num1 = parseInt(prompt("ingrese un numero"));
 let num2 = parseInt(prompt("ingrese otro numero"));
 let num3 = parseInt(prompt("ingrese otro numero"));
-(num1 > num2 && num1 > num3)? alert("el numero 1 es mayor que el numero 2 y el numero 3"): alert("el numero 3 es mayor que el numero 2 y el numero 1") (num2 > num1 && num2 > num3)? alert("el numero 2 es mayor que el numero 1 y el numero 3"): alert("el numero 3 es mayor que el numero 1 y el numero 2") (num3 > num1 && num3 > num2)? alert("el numero 3 es mayor que el numero 1 y el numero 2"): alert("el numero 2 es mayor que el numero 1 y el numero 3");
+(num1 > num2 && num1 > num3)? alert("el numero " + num1 + "es mayor que el numero " + num2 + "y el numero " + num3):
+(num2 > num1 && num2 > num3)? alert("el numero " + num2 + "es mayor que el numero " + num1 + "y el numero " + num3):
+(num3 > num1 && num3 > num2)? alert("el numero " + num3 + "es mayor que el numero " + num1 + "y el numero " + num2):
+alert("los tres numeros son iguales");
